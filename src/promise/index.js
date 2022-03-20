920a231a8b77fcaf9e8266322e435ebbff1508c8
fainnerramirez <fainnerramirez@gmail.com>
2022-03-20 14:30:31 -0500

    Promesas

---
+++ src/promise/index.js
@@ -0,0 +1,38 @@
+const somethingWillHappen = () => {
+  return new Promise((resolve, reject) => {
+    if (true) {
+      resolve("OK!");
+    } else {
+      reject("Whoops!");
+    }
+  });
+};
+
+somethingWillHappen()
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
+
+const somethingWillHappen2 = () => {
+  return new Promise((resolve, reject) => {
+    if (true) {
+      setTimeout(() => {
+        resolve("True");
+      }, 2000);
+    } else {
+      const error = new Error("Whoopps!");
+      reject(error);
+    }
+  });
+};
+
+somethingWillHappen2()
+  .then((response) => console.log(response))
+  .catch((error) => console.error(error));
+
+Promise.all([somethingWillHappen(), somethingWillHappen2()])
+  .then((response) => {
+    console.log("Arrays of results: ", response);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
